73efe02acc509e12edd93098e8e3fa3b10e048af
Stephen882-pixel <stephenondeyo0@gmail.com>
2024-12-31 11:53:49 +0300

    get food,create food and get menus functions

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -30,9 +30,10 @@ func DBinstance() *mongo.Client{
 	return client
 }
 
-var client *mongo.Client =  DBinstance()
+var Client *mongo.Client =  DBinstance()
 
-func openCollection(client *mongo.Client,collectionName string) *mongo.Collection{
+func OpenCollection(client *mongo.Client,collectionName string) *mongo.Collection{
 	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
 	return collection
-} 
+}
+ 
